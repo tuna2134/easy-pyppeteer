c9f2d9cacb3989f354faa32fa66b32ea79cab78d
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-28 18:40:23 +0900

    Update main.py

--- examples/main.py
+++ examples/main.py
@@ -1,9 +1,13 @@
 import pytest
+import asyncio
 from epyppeteer import launch
 
+@pytest.fixture
+def setup():
+    return asyncio.get_running_loop()
 
 @pytest.mark.asyncio
-async def async_gen_fixture():
+async def use_browser(loop):
     async with launch() as browser:
         page = await browser.newPage()
         await page.goto("https://example.com")
