0652877ee0b455bbe5a252acee3eb0bf53a5dc54
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-28 19:05:23 +0900

    Update and rename test_screenshot.py to test_use_browser.py

--- tests/test_use_browser.py
+++ tests/test_use_browser.py
@@ -4,7 +4,9 @@ from epyppeteer import launch
 
 @pytest.mark.asyncio
 async def test_use_browser():
+    print("take example.com")
     async with launch() as browser:
         page = await browser.newPage()
         await page.goto("https://example.com")
+        print(await page.title())
         await page.screenshot({"path": "example.png"})
