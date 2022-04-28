4f00644298813ffdbab77a9d22aef160e2792e50
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-28 20:59:42 +0900

    Update browser.py

--- epyppeteer/browser.py
+++ epyppeteer/browser.py
@@ -1,24 +1,21 @@
-from pyppeteer.browser import Browser as OldBrowser, BrowserContext as OldBrowserContext
+from pyppeteer.browser import Browser, BrowserContext
 from pyppeteer.page import Page
 
 
-class Browser(OldBrowser):
-    
-    async def newContext(self):
-        obj = await self._connection.send('Target.createBrowserContext')
-        browserContextId = obj['browserContextId']
-        context = BrowserContext(self, browserContextId)  # noqa: E501
-        self._contexts[browserContextId] = context
-        return context
+async def newContext(self):
+    obj = await self._connection.send('Target.createBrowserContext')
+    browserContextId = obj['browserContextId']
+    context = BrowserContext(self, browserContextId)  # noqa: E501
+    self._contexts[browserContextId] = context
+    return context
 
+Browser.newContext = newContext
         
-class BrowserContext(OldBrowserContext):
-    async def __aenter__(self) -> Page:
-        return await self.newPage()
+async def __aenter__(self) -> Page:
+    return await self.newPage()
     
-    async def __aexit__(self, *args, **kwargs) -> None:
-        await self.close()
- 
-
-OldBrowserContext = BrowserContext
-OldBrowser = Browser
+async def __aexit__(self, *args, **kwargs) -> None:
+    await self.close()
+    
+BrowserContext.__aenter__ = __aenter__
+BrowserContext.__aexit__ = __aexit__
