3b0895ce9e57f1afe3fde10de6a914907d6c3695
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-28 17:29:24 +0900

    Create browser.py

---
+++ epyppeteer/browser.py
@@ -0,0 +1,15 @@
+from pyppeteer import launch as oldlaunch
+from pyppeteer.browser import Browser
+
+class launch:
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        self.browser: Browser = None
+        
+    async def __aenter__(self) -> Browser:
+        self.browser = await oldlaunch(*args, **kwargs)
+        return self.browser
+    
+    async def __aexit__(self, *args, **kwargs):
+        await self.browser.close()
