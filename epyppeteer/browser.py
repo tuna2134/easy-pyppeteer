45bf5cd3d10391282ab2b4bc48346e979da42a25
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-28 17:48:10 +0900

    Update browser.py

--- epyppeteer/browser.py
+++ epyppeteer/browser.py
@@ -1,14 +1,19 @@
-from pyppeteer import launch as oldlaunch
+from pyppeteer import Launch
 from pyppeteer.browser import Browser
+from typing import Any
 
 class launch:
-    def __init__(self, *args, **kwargs):
-        self.args = args
-        self.kwargs = kwargs
-        self.browser: Browser = None
+    def __init__(self, options: dict, **kwargs: Any):
+        self.__kwargs = kwargs
+        self.__options = options
+        self.__browser: Browser = None
+            
+    @property
+    def browser(self) -> Browser:
+        return self.__browser
         
     async def __aenter__(self) -> Browser:
-        self.browser = await oldlaunch(*self.args, **self.kwargs)
+        self.browser = await Launch().launch(self.__options, **self.__kwargs)
         return self.browser
     
     async def __aexit__(self, *args, **kwargs):
