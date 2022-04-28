e355583e7819881569ddbca5c5595a00d99614f9
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-28 20:30:51 +0900

    Update browser.py

--- epyppeteer/browser.py
+++ epyppeteer/browser.py
@@ -1,5 +1,6 @@
 from pyppeteer.launcher import Launcher
-from pyppeteer.browser import Browser
+from pyppeteer.browser import Browser, BrowserContext as OldBrowserContext
+from pyppeteer.page import Page
 
 
 class launch(Launcher):
@@ -11,5 +12,20 @@ class launch(Launcher):
         self.__browser = await self.launch()
         return self.__browser
     
-    async def __aexit__(self, *args, **kwargs):
+    async def __aexit__(self, *args, **kwargs) -> None:
         await self.__browser.close()
+        
+    async def newContext(self):
+        obj = await self._connection.send('Target.createBrowserContext')
+        browserContextId = obj['browserContextId']
+        context = BrowserContext(self, browserContextId)  # noqa: E501
+        self._contexts[browserContextId] = context
+        return context
+
+        
+class BrowserContext(OldBrowserContext):
+    async def __aenter__(self) -> Page:
+        return await self.newPage()
+    
+    async def __aexit__(self, *args, **kwargs) -> None:
+        await self.close()
